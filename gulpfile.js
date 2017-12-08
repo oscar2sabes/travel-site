bfad603567c80fa7f868736f60be29c5d8ecc4ed
Oscar Figueroa <oscar.figueroa79@gmail.com>
2017-12-08 15:42:32 +1030

    adding updted files

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('default', function(){
+	console.log("Hoorayn - you creted a gulp task");
+});
+
+gulp.task('html', function(){
+	console.log("imagine something useful done to your html here");
+
+});
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+	// body...
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
+	});
+});
